5e7aefb161587198b5feb5fdd397230dbe36bf2a
iravikumar <iravikumar92@gmail.com>
2020-07-05 16:20:54 +0530

    login and logout setted up

--- users/views.py
+++ users/views.py
@@ -1,20 +1,25 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 
-def register(request):
-    if request.method == "POST":
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f"You account has beed created! You are able to log in")
-            return redirect('login')
-    else:
-        form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form': form})
 
-@login_required
-def profile(request):
-    return render(request, 'users/profile.html')
+def login_view(request):
+    print(dict(request.POST))
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        print(username, password, user,'----------------->')
+
+        if user is not None:
+            login(request, user)
+            print('user logged in','----------------->')
+            return redirect('blog-home')
+        else:
+            messages.success(request, f"Your username or password is incorrect, Please try again.")
+            return render(request, 'users/login.html')
+    return render(request, 'users/login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('blog-home')
